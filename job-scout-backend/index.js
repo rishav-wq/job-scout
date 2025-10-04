57d1bcb553c2918e50c6f931079c8dbe2923c9ba
rishav_raj <145801777+rishav-wq@users.noreply.github.com>
2025-10-04 19:06:10 +0530

    Update index.js

--- job-scout-backend/index.js
+++ job-scout-backend/index.js
@@ -31,7 +31,8 @@ app.use(cors({
     origin: [
         'http://localhost:3000',
         'http://localhost:5173',
-        'https://job-scout-i1y9.vercel.app'
+        'https://job-scout-i1y9.vercel.app',
+        'https://job-scout-chi.vercel.app',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
